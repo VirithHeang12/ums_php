29b5da968d4a908cfe20c5d9c6eff8c9ebb87c86
Heang Virith <virith.id@gmail.com>
2024-08-26 15:43:58 +0700

    refactor: Update student deletion functionality

---
+++ authentication/signup.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../database_connection.php';
+
+$dept_code = $_POST['dept_code'] ?? '';
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+$role = $_POST['role'] ?? '';
+
+
